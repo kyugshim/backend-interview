18e70723a4f587bcc41e9cd04acd8ce883df7f49
kyugshim <kyug.shim@gmail.com>
2021-05-04 23:45:13 +0900

    checkIfUserIsOwners.ts'status changed'

--- src/middleware/checkIfUserIsOwner.ts
+++ src/middleware/checkIfUserIsOwner.ts
@@ -1,12 +1,12 @@
-import {Request, Response, NextFunction} from 'express';
+import { Request, Response, NextFunction } from 'express';
 
-export function checkOwner(req: Request, res: Response, next: NextFunction){
+export function checkOwner(req: Request, res: Response, next: NextFunction) {
 
     // Save request data
     const userId = req.params.userId;
     const userReq: any = req.user;
     // Only the same user can modify their own data
-    if(userReq.id != userId) return res.status(403).json({ error: 'Unauthorized access'}); 
+    if (userReq.id != userId) return res.status(401).json({ error: 'Unauthorized access' });
 
     next();
 }
